c6f27b0d26d0ea3234e6aca5cfc3d1379eb42705
Lorel <lorel.oboue@gmail.com>
2022-05-24 17:33:25 +0100

    main task complete

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,6 +1,41 @@
 // TODO: Write your class in this file
+class SoftPlay {
+  constructor(adults = 0, children = 0) {
+    this.adults = adults
+    this.children = children
+  }
+
+  occupancy() {
+    return {
+      children: this.children,
+      adults: this.adults
+    }
+  }
+
+  enter(numAdults, numChildren) {
+    if(numAdults >= numChildren) {
+      this.adults += numAdults
+      this.children += numChildren
+      return true
+    }
+    return false
+  }
+
+  leave(numAdults, numChildren) {
+    if(numAdults < numChildren ||
+      numAdults > this.adults ||  
+      numChildren > this.children ||
+      this.children - numChildren > this.adults - numAdults) {
+        return false
+      }
+    this.adults -= numAdults
+    this.children -= numChildren
+    return true
+  }
+} 
+
 
 // TODO: Change undefined to the name of your class
 module.exports = {
-  SoftPlay: undefined
+  SoftPlay: SoftPlay
 }
